9b6c2a29e0cc290d6ee92356b401c710f528ce46
meowfield <meowfield@yandex.ru>
2024-06-27 01:08:37 +0200

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Данис Гаязов on 27.6.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
